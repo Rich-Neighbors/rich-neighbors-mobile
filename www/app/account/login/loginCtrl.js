7017126adc6de9023656dd3e21d654ee5afd18ac
Casey Woolley <caseywoolley@gmail.com>
2015-12-15 13:31:18 -0600

    added frontside login service and redirect

--- www/app/account/login/loginCtrl.js
+++ www/app/account/login/loginCtrl.js
@@ -1,3 +1,18 @@
-angular.module('app').controller('loginCtrl', function($scope) {
+angular.module('app').controller('loginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+  $scope.data = {};
+
+  $scope.login = function() {
+    console.log('Login user:' + $scope.data.username + ' -ps: ' + $scope.data.password);
+
+    LoginService.loginUser($scope.data.username, $scope.data.password)
+      .success(function(data) {
+        $state.go('tabsController.home');
+      }).error(function(data) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login failed!',
+          template: 'Please check your credentials!'
+        });
+      });
+  };
 
 });
